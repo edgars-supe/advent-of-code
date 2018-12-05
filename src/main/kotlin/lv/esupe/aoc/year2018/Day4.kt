84c1745837a3bcbc54bea425852ee4ab0492cb85
Edgars Supe <edgars.supe@gmail.com>
2018-12-05 23:07:42 +0200

    Clean up day 4

--- src/main/kotlin/lv/esupe/aoc/year2018/Day4.kt
+++ src/main/kotlin/lv/esupe/aoc/year2018/Day4.kt
@@ -1,6 +1,7 @@
 package lv.esupe.aoc.year2018
 
 import lv.esupe.aoc.Puzzle
+import lv.esupe.aoc.utils.benchmark
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -11,78 +12,29 @@ fun main(args: Array<String>) {
 }
 
 class Day4Puzzle1 : Puzzle<Int>(2018, 4, 1) {
-    override fun calculate(): Int {
-        val sleepMap = mutableMapOf<String, MutableList<Int>>()
-        input.mapNotNull {
-            Regex("""\[([0-9-: ]*)] (.*)""")
-                .find(it)
-                ?.groupValues
-        }
-            .map { Log(SimpleDateFormat("yyyy-MM-dd hh:mm").parse(it[1]), it[2]) }
-            .sortedBy { it.date }
-            .apply {
-                var currentId: String = "-1"
-                var sleepStartTime = -1
-                forEach {
-                    when (it.entry) {
-                        is Entry.Start -> currentId = it.entry.id
-                        is Entry.Sleep -> sleepStartTime = it.date.minutes
-                        is Entry.Wake -> {
-                            for (i in sleepStartTime until it.date.minutes) {
-                                val list = sleepMap.getOrDefault(currentId, getMinuteList())
-                                list[i]++
-                                sleepMap[currentId] = list
-                            }
-                        }
-                    }
-                }
-            }
-        return sleepMap.maxBy { it.value.sum() }
+    override fun calculate(): Int =
+        input.toSleepMap()
+            .maxBy { it.value.sum() }
             ?.let { it.key.toInt() * it.value.indexOf(it.value.max()!!) }
             ?: throw Exception("Grinch stole Christmas")
-    }
 }
 
 class Day4Puzzle2 : Puzzle<Int>(2018, 4, 2) {
-    override fun calculate(): Int {
-        val sleepMap = mutableMapOf<String, MutableList<Int>>()
-        input.mapNotNull {
-            Regex("""\[([0-9-: ]*)] (.*)""")
-                .find(it)
-                ?.groupValues
-        }
-            .map { Log(SimpleDateFormat("yyyy-MM-dd hh:mm").parse(it[1]), it[2]) }
-            .sortedBy { it.date }
-            .apply {
-                var currentId: String = "-1"
-                var sleepStartTime = -1
-                forEach {
-                    when (it.entry) {
-                        is Entry.Start -> currentId = it.entry.id
-                        is Entry.Sleep -> sleepStartTime = it.date.minutes
-                        is Entry.Wake -> {
-                            for (i in sleepStartTime until it.date.minutes) {
-                                val list = sleepMap.getOrDefault(currentId, getMinuteList())
-                                list[i]++
-                                sleepMap[currentId] = list
-                            }
-                        }
-                    }
-                }
-            }
-        return sleepMap.maxBy { it.value.max()!! }
+    override fun calculate(): Int =
+        input.toSleepMap()
+            .maxBy { it.value.max()!! }
             ?.let { it.key.toInt() * it.value.indexOf(it.value.max()!!) }
             ?: throw Exception("Grinch stole Christmas")
-    }
 }
 
 class Log(
-    val date: Date,
+    val date: String,
     entry: String
 ) {
+    val minutes = date.takeLast(2).toInt()
     val entry: Entry = entry.parseEntry()
 
-    fun String.parseEntry(): Entry =
+    private fun String.parseEntry(): Entry =
         when {
             this == "wakes up" -> Entry.Wake()
             this == "falls asleep" -> Entry.Sleep()
@@ -99,5 +51,36 @@ sealed class Entry {
 
 private fun getMinuteList(): MutableList<Int> =
     mutableListOf<Int>().apply {
-        for (i in 0 .. 59) add(0)
+        for (i in 0..59) add(0)
     }
+
+private fun String.toLog(): Log? =
+    Regex("""\[([0-9-: ]*)] (.*)""")
+        .find(this)
+        ?.groupValues
+        ?.let { Log(it[1], it[2]) }
+
+private fun List<String>.toSleepMap() =
+    mapNotNull { it.toLog() }
+        .sortedBy { it.date }
+        .mapSleep()
+
+private fun List<Log>.mapSleep(): Map<String, List<Int>> {
+    val sleepMap = mutableMapOf<String, MutableList<Int>>()
+    var currentId = "-1"
+    var sleepStartTime = -1
+    forEach {
+        when (it.entry) {
+            is Entry.Start -> currentId = it.entry.id
+            is Entry.Sleep -> sleepStartTime = it.minutes
+            is Entry.Wake -> {
+                for (i in sleepStartTime until it.minutes) {
+                    val list = sleepMap.getOrDefault(currentId, getMinuteList())
+                    list[i]++
+                    sleepMap[currentId] = list
+                }
+            }
+        }
+    }
+    return sleepMap
+}
