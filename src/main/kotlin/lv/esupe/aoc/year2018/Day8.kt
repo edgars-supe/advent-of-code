d0b4955778fc67df7ab1e59fe3af4bbcbe35db69
Edgars Supe <edgars.supe@gmail.com>
2018-12-08 13:52:35 +0200

    Day 8 solution

---
+++ src/main/kotlin/lv/esupe/aoc/year2018/Day8.kt
@@ -0,0 +1,51 @@
+package lv.esupe.aoc.year2018
+
+import lv.esupe.aoc.Puzzle
+import lv.esupe.aoc.solve
+
+
+fun main(args: Array<String>) = solve { Day8() }
+
+class Day8 : Puzzle<Int, Int>(2018, 8) {
+    override val input = rawInput.first().split(" ").map { it.toInt() }
+    val nodes = mutableListOf<Node>()
+
+    init {
+        createNodes(null, input)
+    }
+
+    override fun solvePartOne(): Int = nodes.map { it.metadata.sum() }.sum()
+
+    override fun solvePartTwo(): Int = nodes.first().value()
+
+    /**
+     * Creates a node, assuming `input[0]` is the child count and `input[1]` is metadata count.
+     * Adds nodes to [nodes].
+     *
+     * @return Offset of first metadata entry for parent Node
+     */
+    fun createNodes(parent: Node?, input: List<Int>): Int {
+        val node = Node()
+        val childCount = input[0]
+        val metadataCount = input[1]
+        parent?.children?.add(node)
+        nodes.add(node)
+        var offset = 2
+        for (i in 0 until childCount) {
+            offset += createNodes(node, input.drop(offset))
+        }
+        for (i in 0 until metadataCount) {
+            node.metadata.add(input[offset++])
+        }
+        return offset
+    }
+
+    class Node {
+        val children = mutableListOf<Node>()
+        val metadata = mutableListOf<Int>()
+
+        fun value(): Int =
+            if (children.isEmpty()) metadata.sum()
+            else metadata.mapNotNull { child -> children.getOrNull(child - 1)?.value() }.sum()
+    }
+}
