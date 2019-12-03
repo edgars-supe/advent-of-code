ea4d5a7bf9ed9b8f60bf2de6b56c4b0c3246d74f
Edgars Supe <edgars.supe@gmail.com>
2019-12-03 22:05:59 +0200

    Clean up result printing and benchmarking

--- src/main/kotlin/lv/esupe/aoc/Puzzle.kt
+++ src/main/kotlin/lv/esupe/aoc/Puzzle.kt
@@ -2,11 +2,9 @@ package lv.esupe.aoc
 
 import java.nio.file.Files
 import java.nio.file.Paths
-import kotlin.system.measureNanoTime
 
 
-abstract class Puzzle<T, U>(year: Int, day: Int) {
-    val identifier = "y${year}d$day"
+abstract class Puzzle<T, U>(val year: Int, val day: Int) {
     val rawInput = getInput(year, day)
     abstract val input: Any
 
@@ -20,35 +18,3 @@ abstract class Puzzle<T, U>(year: Int, day: Int) {
             .let { Paths.get(it) }
             .let { Files.readAllLines(it) }
 }
-
-fun solve(block: () -> Puzzle<*, *>) {
-    val puzzle = block()
-    val partOneResult = puzzle.solvePartOne()
-    val partTwoResult = puzzle.solvePartTwo()
-    var initTime = 0L
-    var partOneTime = 0L
-    var partTwoTime = 0L
-    repeat(5) {
-        lateinit var p: Puzzle<*, *>
-        initTime += measureNanoTime { p = block() }
-        partOneTime += measureNanoTime { p.solvePartOne() }
-        partTwoTime += measureNanoTime { p.solvePartTwo() }
-    }
-    initTime /= 5
-    partOneTime /= 5
-    partTwoTime /= 5
-    println(String.format("${puzzle.identifier}init: %.3fms (${initTime}ns)", initTime.toMillis()))
-    printPartResult(puzzle, 1, partOneResult, partOneTime, initTime)
-    printPartResult(puzzle, 2, partTwoResult, partTwoTime, initTime)
-}
-
-private fun printPartResult(puzzle: Puzzle<*, *>, part: Int, result: Any?, time: Long, initTime: Long) {
-    println(String.format(
-        "${puzzle.identifier}p$part: $result, took %.3fms (${time}ns), incl. init: %.3fms (${initTime}ns)",
-        time.toMillis(),
-        time.toMillis() + initTime.toMillis()
-    ))
-}
-
-
-private fun Long.toMillis() = this / 1000000f
---
+++ src/main/kotlin/lv/esupe/aoc/Solver.kt
@@ -0,0 +1,60 @@
+package lv.esupe.aoc
+
+import kotlin.system.measureNanoTime
+
+fun <T : Any, R : Any> solve(block: () -> Puzzle<T, R>) {
+    printResult(block)
+    println()
+    benchmark(block)
+}
+
+private fun <T : Any, R : Any> printResult(block: () -> Puzzle<T, R>) {
+    val puzzle = block()
+    val partOneResult = puzzle.solvePartOne().bold("33")
+    val partTwoResult = puzzle.solvePartTwo().bold("33")
+    println("Year ${puzzle.year}, Day ${puzzle.day}".bold())
+    println("Part 1: $partOneResult")
+    println("Part 2: $partTwoResult")
+}
+
+private fun benchmark(block: () -> Puzzle<*, *>) {
+    println("Benchmarking...")
+    var initTime = 0L
+    var partOneTime = 0L
+    var partTwoTime = 0L
+    repeat(5) {
+        lateinit var p: Puzzle<*, *>
+        initTime += measureNanoTime { p = block() }
+        partOneTime += measureNanoTime { p.solvePartOne() }
+        partTwoTime += measureNanoTime { p.solvePartTwo() }
+    }
+    initTime /= 5
+    partOneTime /= 5
+    partTwoTime /= 5
+
+    printBenchmark(initTime.toMillis(), partOneTime.toMillis(), partTwoTime.toMillis())
+}
+
+private fun printBenchmark(initTime: Float, partOneTime: Float, partTwoTime: Float) {
+    println("Initialization took " + "%.3fms".format(initTime).bold())
+    printPartBenchmark(1, partOneTime, initTime)
+    printPartBenchmark(2, partTwoTime, initTime)
+    println("Total: " + "%.3fms".format(initTime + partOneTime + partTwoTime).bold())
+}
+
+private fun printPartBenchmark(part: Int, time: Float, initTime: Float) {
+    println(
+        "Part $part took " +
+            "%.3fms".format(time).bold() +
+            ", including init: " +
+            "%.3fms".format((initTime + time)).bold()
+    )
+}
+
+private fun Any.bold(color: String = ""): String = style("$color;1")
+
+private fun Any.style(color: String): String {
+    return "\u001B[${color}m$this\u001B[0m"
+}
+
+private fun Long.toMillis() = this / 1000000f
