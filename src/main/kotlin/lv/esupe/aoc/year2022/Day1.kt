6ca4bb6b32a80609ff6b936d1e4ae0162743f42e
Edgars Supe <edgars.supe@gmail.com>
2022-12-02 20:00:26 +0200

    2022, Day 1: Fixes

--- src/main/kotlin/lv/esupe/aoc/year2022/Day1.kt
+++ src/main/kotlin/lv/esupe/aoc/year2022/Day1.kt
@@ -4,21 +4,22 @@ import lv.esupe.aoc.Puzzle
 import lv.esupe.aoc.solve
 import lv.esupe.aoc.utils.chunkedBy
 
-fun main() = solve(benchmark = false) { Day1() }
+fun main() = solve { Day1() }
 
-class Day1 : Puzzle<Any, Any>(2022, 1) {
+class Day1 : Puzzle<Int, Int>(2022, 1) {
     override val input = rawInput
         .chunkedBy { it.isBlank() }
         .map { elfCalories ->
             elfCalories.sumOf { it.toInt() }
         }
+        .sortedDescending()
 
     override fun solvePartOne(): Int {
-        return input.max()
+        return input.first()
     }
 
     override fun solvePartTwo(): Int {
-        return input.sortedDescending()
+        return input
             .take(3)
             .sum()
     }
--- src/test/kotlin/lv/esupe/aoc/year2022/Day1Test.kt
+++ src/test/kotlin/lv/esupe/aoc/year2022/Day1Test.kt
@@ -3,7 +3,7 @@ package lv.esupe.aoc.year2022
 import lv.esupe.aoc.DayTest
 import org.junit.jupiter.api.Test
 
-class Day1Test : DayTest<Any, Any>() {
+class Day1Test : DayTest<Int, Int>() {
     override val puzzle = { Day1() }
 
     @Test
