890566608ef9c042f3a721b62c89c4f40b0c7cbb
Edgars Supe <edgars.supe@gmail.com>
2022-12-07 19:41:35 +0200

    2022, Day 7: Optimize

--- src/main/kotlin/lv/esupe/aoc/year2022/Day7.kt
+++ src/main/kotlin/lv/esupe/aoc/year2022/Day7.kt
@@ -9,76 +9,78 @@ class Day7 : Puzzle<Int, Int>(2022, 7) {
 
     override val input = rawInput.drop(1)
 
-    private val rootDir: Node.Dir
+    private val rootDir: Dir = Dir("/", mutableListOf(), null)
 
     init {
-        rootDir = Node.Dir("/", mutableListOf(), null)
         var currentDir = rootDir
         input.forEach { line ->
             if (line.startsWith('$')) {
-                val l = line.split(' ')
-                if (l[1] == "cd") {
-                    currentDir = if (l[2] != "..") {
-                        currentDir.children.first { it is Node.Dir && it.name == l[2] } as Node.Dir
-                    } else {
-                        currentDir.parent!!
-                    }
-                }
+                parseInput(line, currentDir)?.let { currentDir = it }
             } else {
-                val l = line.split(' ')
-                if (l[0] == "dir") {
-                    currentDir.children.add(Node.Dir(l[1], mutableListOf(), currentDir))
-                } else { // file
-                    val size = l[0].toInt()
-                    currentDir.children.add(Node.File(l[1], size))
-                }
+                parseOutput(line, currentDir)
             }
+        }
+    }
+
+    private fun parseInput(line: String, currentDir: Dir): Dir? {
+        val l = line.split(' ')
+        if (l[1] != "cd") return null
+
+        return if (l[2] == "..") {
+            currentDir.parent!!
+        } else {
+            currentDir.children.first { it.name == l[2] }
+        }
+    }
+
+    private fun parseOutput(line: String, currentDir: Dir) {
+        val l = line.split(' ')
+        if (l[0] == "dir") { // "dir foobar"
+            currentDir.children.add(Dir(l[1], mutableListOf(), currentDir))
+        } else { // "12345 foobar"
+            val size = l[0].toInt()
+            currentDir.addFile(size)
         }
     }
 
     override fun solvePartOne(): Int {
         var smallDirTotalSize = 0
         traverseDirs { dir ->
-            if (dir.size() <= PART_1_THRESHOLD) smallDirTotalSize += dir.size()
+            if (dir.totalSize <= PART_1_THRESHOLD) smallDirTotalSize += dir.totalSize
         }
         return smallDirTotalSize
     }
 
     override fun solvePartTwo(): Int {
-        val freeSpace = TOTAL_SPACE - rootDir.size()
+        val freeSpace = TOTAL_SPACE - rootDir.totalSize
         val toDelete = REQUIRED_FREE_SPACE - freeSpace
         var minSize = Int.MAX_VALUE
-        traverseDirs(filter = { dir -> dir.size() > toDelete }) { dir ->
-            if (dir.size() < minSize) minSize = dir.size()
+        traverseDirs(filter = { dir -> dir.totalSize > toDelete }) { dir ->
+            if (dir.totalSize < minSize) minSize = dir.totalSize
         }
         return minSize
     }
 
-    private fun traverseDirs(filter: (Node) -> Boolean = { true }, onEach: (Node.Dir) -> Unit) {
-        val queue = ArrayDeque<Node>()
+    private fun traverseDirs(filter: (Dir) -> Boolean = { true }, onEach: (Dir) -> Unit) {
+        val queue = ArrayDeque<Dir>()
         queue.add(rootDir)
         while (queue.isNotEmpty()) {
             val current = queue.removeLast()
-            if (current is Node.Dir) {
-                val dirs = current.children.filter(filter)
-                queue.addAll(dirs)
-                onEach(current)
-            }
+            val dirs = current.children.filter(filter)
+            queue.addAll(dirs)
+            onEach(current)
         }
     }
 
-    sealed class Node {
-        data class Dir(val name: String, val children: MutableList<Node>, val parent: Dir?) : Node() {
-            private val _size: Int by lazy { children.sumOf { it.size() } }
+    class Dir(val name: String, val children: MutableList<Dir>, val parent: Dir?) {
+        var fileSize: Int = 0
+            private set
 
-            override fun size(): Int = _size
+        val totalSize: Int by lazy { children.sumOf { it.totalSize } + fileSize }
+
+        fun addFile(size: Int) {
+            fileSize += size
         }
-
-        data class File(val name: String, val size: Int): Node() {
-            override fun size(): Int = size
-        }
-
-        abstract fun size(): Int
     }
 
     companion object {
