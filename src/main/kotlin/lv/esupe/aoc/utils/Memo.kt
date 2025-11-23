6faaf64ae0e08bb625bc6d129a33d8e8e2de5012
Edgars Supe <edgars.supe@gmail.com>
2025-11-23 22:58:14 +0200

    Fix memoization function

--- src/main/kotlin/lv/esupe/aoc/utils/Memo.kt
+++ src/main/kotlin/lv/esupe/aoc/utils/Memo.kt
@@ -2,10 +2,10 @@ package lv.esupe.aoc.utils
 
 fun <T, R> memoize(function: (T) -> R): (T) -> R {
     val cache = mutableMapOf<T, R>()
-    return { t -> cache.computeIfAbsent(t, function) }
+    return { t -> cache.getOrPut(t) { function(t) } }
 }
 
 fun <T, R, V> memoize(function: (T, R) -> V): (T, R) -> V {
     val cache = mutableMapOf<Pair<T, R>, V>()
-    return { t, r -> cache.computeIfAbsent(t to r) { (t, r) -> function(t, r) } }
+    return { t, r -> cache.getOrPut(t to r) { function(t, r) } }
 }
